ee02cbcee035fdf8347a56199543a82c855e9064
Samer Al3oory <samer@domedia.me>
2023-09-22 23:38:20 +0300

    added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,26 @@
-import { Button, Grid, GridItem,  } from '@chakra-ui/react'
+import { Button, Grid, GridItem, Show,  } from '@chakra-ui/react'
 
 import "./App.css";
 
 function App() {
   
   return (
-    <Grid
-    templateAreas={`"navbar navbar"
-                    "sidebar"
-                    "content"`}
-    gridTemplateRows={'1fr 1fr 30px'}
-    gridTemplateColumns={'150px 1fr'}
-    h='200px'
-    gap='1'
-    color='blackAlpha.700'
-    fontWeight='bold'
-  >
-    <Button>Hi</Button>
-    <GridItem pl='2' bg='orange.300' area={'navbar'}>
-      NavBar
+ <Grid templateAreas={{
+  base: `"nav" "main"`,
+  lg: `"nav nav" "aside main"` //1024px
+ }}>
+  <GridItem area={"nav"} bg={'coral'}>
+    Nav
+  </GridItem>
+  <Show above='lg'>
+    <GridItem area={"aside"} bg={'gold'}>
+      aside
     </GridItem>
-    <GridItem pl='2' bg='pink.300' area={'sidebar'}>
-      Nav
-    </GridItem>
-    <GridItem pl='2' bg='green.300' area={'content'}>
-      Main
-    </GridItem>
-  </Grid>
+  </Show>
+  <GridItem area={"main"} bg={'dodgerblue'}>
+    main
+  </GridItem>
+ </Grid>
   );
 }
 
