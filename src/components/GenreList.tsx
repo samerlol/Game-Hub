1c17daff38b3dbcadf798420d88d92f5ffff546d
Samer Al3oory <samer@domedia.me>
2023-10-27 02:17:31 +0300

    Highlight selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,8 +3,9 @@ import useGenres, { Genre } from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url';
 interface Props{
   onSelectGenre: (genre: Genre)=> void;
+  selectedGenre: Genre | null;
 }
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data, isLoading, error} = useGenres();
   if(error)
     return null;
@@ -12,10 +13,10 @@ const GenreList = ({ onSelectGenre }: Props) => {
         return <Spinner />
     return (
         <List>
-        {data.map(genre => <ListItem key={genre.id} paddingY={1}>
+        {data.map(genre => <ListItem key={genre.id} paddingY={genre.id === selectedGenre?.id? 4:1}>
             <HStack>
                 <Image src={getCroppedImageUrl(genre.image_background)} boxSize={'32px'} borderRadius={10} alt='img'/>
-                <Button fontSize='lg' variant={'link'} whiteSpace={'normal'} onClick={()=>onSelectGenre(genre)}>{genre.name}</Button>
+                <Button fontWeight={genre.id === selectedGenre?.id?'bold': 'normal'} fontSize='lg' variant={'link'} whiteSpace={'normal'} onClick={()=>onSelectGenre(genre)}>{genre.name}</Button>
             </HStack>
             </ListItem>)}
     </List>
