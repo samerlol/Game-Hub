0d33098116f838a63627da3a57210e2f86684b53
Samer Al3oory <samer@domedia.me>
2024-01-26 20:11:36 +0300

    Handle errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import { GameDetailsPage } from "../pages/GameDetailsPage";
 import { HomePage } from "../pages/HomePage";
 import Layout from "../pages/Layout";
+import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
     { 
         path: '/', 
         element: <Layout />,
+        errorElement: <ErrorPage/>,
         children: [
             {
                 index: true,
