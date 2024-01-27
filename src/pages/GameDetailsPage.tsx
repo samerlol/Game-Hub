fc4c449c70a690efcd56d67a5bc4a0f69438c772
Samer Al3oory <samer@domedia.me>
2024-01-27 11:11:01 +0300

    Formatting

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -2,9 +2,9 @@ import { GridItem, Heading, SimpleGrid, Spinner } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
-import useGame from '../hooks/useGame';
-import GameTrailer from '../components/GameTrailer';
 import GameScreenshots from '../components/GameScreenshots';
+import GameTrailer from '../components/GameTrailer';
+import useGame from '../hooks/useGame';
 
 export const GameDetailsPage = () => {
   const { slug } = useParams();
